e2e8241a2da9e0e75f8bbe2eb5dbabf3e37723b4
Arpit Subhash <103520728+Kashi1311@users.noreply.github.com>
2023-04-21 22:02:43 +0530

    created 168.cpp
    Flatten Nested List Iterator

---
+++ tut168.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+class NestedIterator {
+ public:
+  NestedIterator(vector<NestedInteger>& nestedList) {
+    addInteger(nestedList);
+  }
+
+  int next() {
+    const int num = q.front();
+    q.pop();
+    return num;
+  }
+
+  bool hasNext() {
+    return !q.empty();
+  }
+
+ private:
+  queue<int> q;
+
+  void addInteger(const vector<NestedInteger>& nestedList) {
+    for (const NestedInteger& ni : nestedList)
+      if (ni.isInteger())
+        q.push(ni.getInteger());
+      else
+        addInteger(ni.getList());
+  }
+};
