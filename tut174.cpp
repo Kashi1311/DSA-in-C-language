d23bc7b1fe3382875872f12c5c4293104158233d
Arpit Subhash <103520728+Kashi1311@users.noreply.github.com>
2023-04-25 23:15:45 +0530

    created tut174.cpp and tut175.cpp

---
+++ tut174.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+ public:
+  vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    vector<vector<int>> ans;
+
+    sort(begin(intervals), end(intervals));
+
+    for (const vector<int>& interval : intervals)
+      if (ans.empty() || ans.back()[1] < interval[0])
+        ans.push_back(interval);
+      else
+        ans.back()[1] = max(ans.back()[1], interval[1]);
+
+    return ans;
+  }
+};
