1e3cc1447567b3da1b8703d187ddb7aa08f48c31
Arpit Subhash <103520728+Kashi1311@users.noreply.github.com>
2023-05-16 23:09:22 +0530

    created 203.cpp
    Repeated DNA Sequences Bit manipulation problem

---
+++ tut1203.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<unordered_set>
+#include<string.h>
+using namespace std;
+
+class Solution {
+ public:
+  vector<string> findRepeatedDnaSequences(string s) {
+    unordered_set<string> ans;
+    unordered_set<string_view> seen;
+    const string_view sv(s);
+
+    for (int i = 0; i + 10 <= s.length(); ++i) {
+      if (seen.count(sv.substr(i, 10)))
+        ans.insert(s.substr(i, 10));
+      seen.insert(sv.substr(i, 10));
+    }
+
+    return {begin(ans), end(ans)};
+  }
+};
