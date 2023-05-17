7ad2de483850113db99ea03e5192bd362453df89
Arpit Subhash <103520728+Kashi1311@users.noreply.github.com>
2023-05-17 23:03:55 +0530

    created tut206.cpp
    BITWISE AND numbers range - Bit manipulation problem

---
+++ tut206.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+ public:
+  int rangeBitwiseAnd(int m, int n) {
+    int shiftBits = 0;
+
+    while (m != n) {
+      m >>= 1;
+      n >>= 1;
+      ++shiftBits;
+    }
+
+    return m << shiftBits;
+  }
+};
