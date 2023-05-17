13b62971f5adc99733766018b49d544731e705c3
Arpit Subhash <103520728+Kashi1311@users.noreply.github.com>
2023-05-17 23:01:35 +0530

    updated tut205.cpp

---
+++ tut205.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+ public:
+  int hammingWeight(uint32_t n) {
+    int ans = 0;
+
+    for (int i = 0; i < 32; ++i)
+      if ((n >> i) & 1)
+        ++ans;
+
+    return ans;
+  }
+};
