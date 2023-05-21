a9f1d2d9a86079ba28968a002c1f9deef98de34b
Arpit Subhash <103520728+Kashi1311@users.noreply.github.com>
2023-05-21 22:10:59 +0530

    created tut221.cpp
    Clone Graph - Hash Table Problem
    Approach - 1 BFS

---
+++ tut221.cpp
@@ -0,0 +1,30 @@
+// Approach - 1 BFS //
+#include<iostream>
+#include<queue>
+#include<unordered_map>
+using namespace std;
+
+class Solution {
+ public:
+  Node* cloneGraph(Node* node) {
+    if (node == nullptr)
+      return nullptr;
+
+    queue<Node*> q{{node}};
+    unordered_map<Node*, Node*> map{{node, new Node(node->val)}};
+
+    while (!q.empty()) {
+      Node* u = q.front();
+      q.pop();
+      for (Node* v : u->neighbors) {
+        if (!map.count(v)) {
+          map[v] = new Node(v->val);
+          q.push(v);
+        }
+        map[u]->neighbors.push_back(map[v]);
+      }
+    }
+
+    return map[node];
+  }
+};
