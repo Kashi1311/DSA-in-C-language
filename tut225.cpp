327213da5e648662dfb839f43346b72dc443d153
Arpit Subhash <103520728+Kashi1311@users.noreply.github.com>
2023-05-22 22:08:20 +0530

    created tut225.cpp
    Word Break - Hash Table Problem
    Approach - 3 Bottom-up

---
+++ tut225.cpp
@@ -0,0 +1,29 @@
+// Approach - 3 Bottom-up//
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+#include<unordered_set>
+using namespace std;
+
+class Solution {
+ public:
+  bool wordBreak(string s, vector<string>& wordDict) {
+    const int n = s.length();
+    unordered_set<string> wordSet{begin(wordDict), end(wordDict)};
+    // dp[i] := true if s[0..i) can be segmented
+    vector<bool> dp(n + 1);
+    dp[0] = true;
+
+    for (int i = 1; i <= n; ++i)
+      for (int j = 0; j < i; ++j)
+        // s[0..j) can be segmented and s[j..i) in wordSet
+        // So s[0..i) can be segmented
+        if (dp[j] && wordSet.count(s.substr(j, i - j))) {
+          dp[i] = true;
+          break;
+        }
+
+    return dp[n];
+  }
+};
+
