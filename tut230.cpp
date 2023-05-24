003b89da22f9be33524ea38464013df41ff4d922
Arpit Subhash <103520728+Kashi1311@users.noreply.github.com>
2023-05-24 21:38:04 +0530

    Created 230.cpp
    Intersection of two linked lists - Hash Table Problem

---
+++ tut230.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<list>
+using namespace std;
+
+class Solution {
+ public:
+  ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+    ListNode* a = headA;
+    ListNode* b = headB;
+
+    while (a != b) {
+      a = a ? a->next : headB;
+      b = b ? b->next : headA;
+    }
+
+    return a;
+  }
+};
