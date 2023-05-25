28527f5a997ab45f0738ea696d86c41a66685602
Arpit Subhash <103520728+Kashi1311@users.noreply.github.com>
2023-05-25 22:28:07 +0530

    Created tut233.cpp
    Best time to buy and sell stock 2 - Greedy Problem

---
+++ tut233.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+
+class Solution {
+ public:
+  int maxProfit(vector<int>& prices) {
+    int sell = 0;
+    int hold = INT_MIN;
+
+    for (const int price : prices) {
+      sell = max(sell, hold + price);
+      hold = max(hold, sell - price);
+    }
+
+    return sell;
+  }
+};
