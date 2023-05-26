4a49d24b340100668d9ae24606ce62f6ab466113
Arpit Subhash <103520728+Kashi1311@users.noreply.github.com>
2023-05-26 22:06:03 +0530

    Created tut239.cpp
    Validate Binary Search Tree - Tree Problem
    Approach 2 - Iterative(Stack)

---
+++ tut239.cpp
@@ -0,0 +1,26 @@
+// Approch 2 - Iterative(Stack) //
+#include<iostream>
+#include<stack>
+using namespace std;
+
+class Solution {
+ public:
+  bool isValidBST(TreeNode* root) {
+    stack<TreeNode*> stack;
+    TreeNode* pred = nullptr;
+
+    while (root || !stack.empty()) {
+      while (root) {
+        stack.push(root);
+        root = root->left;
+      }
+      root = stack.top(), stack.pop();
+      if (pred && pred->val >= root->val)
+        return false;
+      pred = root;
+      root = root->right;
+    }
+
+    return true;
+  }
+};
