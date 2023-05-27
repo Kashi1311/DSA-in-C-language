db4544f08769a881ab9903bc66182eafcdb85857
Arpit Subhash <103520728+Kashi1311@users.noreply.github.com>
2023-05-27 23:17:16 +0530

    Created tut246.cpp
    Binary Tree Side Review - Tree Problem
    Approach 2 - DFS

---
+++ tut246.cpp
@@ -0,0 +1,24 @@
+// Approach 2 - DFS //
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+ public:
+  vector<int> rightSideView(TreeNode* root) {
+    vector<int> ans;
+    dfs(root, 0, ans);
+    return ans;
+  }
+
+ private:
+  void dfs(TreeNode* root, int depth, vector<int>& ans) {
+    if (root == nullptr)
+      return;
+
+    if (depth == ans.size())
+      ans.push_back(root->val);
+    dfs(root->right, depth + 1, ans);
+    dfs(root->left, depth + 1, ans);
+  }
+};
