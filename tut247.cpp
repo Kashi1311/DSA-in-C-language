2c59a302876f745719601dda4d338fca2812f0f1
Arpit Subhash <103520728+Kashi1311@users.noreply.github.com>
2023-05-27 23:22:21 +0530

    Created tut247.cpp
    Count Complete Tree Nodes - Tree Problem
    Approach 1 - Naive

---
+++ tut247.cpp
@@ -0,0 +1,12 @@
+// Approach 1 - Naive //
+#include<iostream>
+using namespace std;
+
+class Solution {
+ public:
+  int countNodes(TreeNode* root) {
+    if (root == nullptr)
+      return 0;
+    return 1 + countNodes(root->left) + countNodes(root->right);
+  }
+};
