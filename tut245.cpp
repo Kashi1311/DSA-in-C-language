4d8ddc67b0f946cf50da4c6916f3bd19e8b3850d
Arpit Subhash <103520728+Kashi1311@users.noreply.github.com>
2023-05-27 23:12:10 +0530

    Created tut245.cpp
    Binary Tree Right Side View - Tree Problem
    Approach 1 - BFS

---
+++ tut245.cpp
@@ -0,0 +1,32 @@
+// Approach 1 - BFS //
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+class Solution {
+ public:
+  vector<int> rightSideView(TreeNode* root) {
+    if (root == nullptr)
+      return {};
+
+    vector<int> ans;
+    queue<TreeNode*> q{{root}};
+
+    while (!q.empty()) {
+      const int size = q.size();
+      for (int i = 0; i < size; ++i) {
+        TreeNode* node = q.front();
+        q.pop();
+        if (i == size - 1)
+          ans.push_back(node->val);
+        if (node->left)
+          q.push(node->left);
+        if (node->right)
+          q.push(node->right);
+      }
+    }
+
+    return ans;
+  }
+};
