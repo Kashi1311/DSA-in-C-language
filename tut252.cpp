f0562d6867de41082a970c006715836971234e3f
Arpit Subhash <103520728+Kashi1311@users.noreply.github.com>
2023-05-28 22:03:51 +0530

    Created tut252.cpp
    Kth Smallest Element un BST - Tree Problem
    Approach 3 - Stack

---
+++ tut252.cpp
@@ -0,0 +1,27 @@
+// Approach 3 - Stack //
+#include<iostream>
+#include<stack>
+using namespace std;
+
+class Solution {
+ public:
+  int kthSmallest(TreeNode* root, int k) {
+    stack<TreeNode*> stack;
+
+    while (root) {
+      stack.push(root);
+      root = root->left;
+    }
+
+    for (int i = 0; i < k - 1; ++i) {
+      root = stack.top(), stack.pop();
+      root = root->right;
+      while (root) {
+        stack.push(root);
+        root = root->left;
+      }
+    }
+
+    return stack.top()->val;
+  }
+};
