24b3110f747c037feb8a53beb0c046170341679e
Arpit Subhash <103520728+Kashi1311@users.noreply.github.com>
2023-06-04 22:41:20 +0530

    Created tut285.cpp
    Find K closest elements - Sliding Window Challenge

---
+++ tut285.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+ public:
+  vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+    int l = 0;
+    int r = arr.size() - k;
+
+    while (l < r) {
+      const int m = (l + r) / 2;
+      if (x - arr[m] <= arr[m + k] - x)
+        r = m;
+      else
+        l = m + 1;
+    }
+
+    return {begin(arr) + l, begin(arr) + l + k};
+  }
+};
