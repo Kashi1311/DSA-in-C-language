67a65962521c98f9fff765febf6568c70183082d
Arpit Subhash <103520728+Kashi1311@users.noreply.github.com>
2023-06-05 22:47:44 +0530

    Create tut290.cpp
    Binary Subarrays with sum - Sliding Window Challenge

---
+++ tut290.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+using namespace std;
+
+class Solution {
+ public:
+  int numSubarraysWithSum(vector<int>& nums, int goal) {
+    int ans = 0;
+    int prefix = 0;
+    unordered_map<int, int> count{{0, 1}};
+
+    for (int num : nums) {
+      prefix += num;
+      if (const auto it = count.find(prefix - goal); it != cend(count))
+        ans += it->second;
+      ++count[prefix];
+    }
+
+    return ans;
+  }
+};
