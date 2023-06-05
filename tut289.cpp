147a5ee8b4e821fd037d546c13a005376fa98a5f
Arpit Subhash <103520728+Kashi1311@users.noreply.github.com>
2023-06-05 22:44:39 +0530

    Created tut289.cpp
    Fruit Into Baskets - Sliding Window Challenge

---
+++ tut289.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+class Solution {
+ public:
+  int totalFruit(vector<int>& fruits) {
+    int ans = 0;
+    unordered_map<int, int> count;
+
+    for (int l = 0, r = 0; r < fruits.size(); ++r) {
+      ++count[fruits[r]];
+      while (count.size() > 2) {
+        if (--count[fruits[l]] == 0)
+          count.erase(fruits[l]);
+        ++l;
+      }
+      ans = max(ans, r - l + 1);
+    }
+
+    return ans;
+  }
+};
